a9227db40ec6e3a0297dc332fea511419b0f2e20
stefan <kannisko@gmail.com>
2020-01-20 21:01:44 +0100

    pierwszy albo i nie

--- firstPackage/testfile.py
+++ firstPackage/testfile.py
@@ -2,4 +2,12 @@ import numpy as np
 sentence = """Thomas Jefferson began building Monticello at the age of 26."""
 token_sequence = str.split(sentence)
 vocab = sorted(set(token_sequence))
+num_tokens = len(token_sequence)
+vocab_size = len(vocab)
+onehot_vectors = np.zeros((num_tokens,vocab_size), int)
+for i, word in enumerate(token_sequence):
+    onehot_vectors[i, vocab.index(word)] = 1
+print(onehot_vectors)
+import pandas as pd
+print(pd.DataFrame(onehot_vectors, columns=vocab))
 print("xx")
